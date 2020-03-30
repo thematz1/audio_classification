93b0c69f3b8bf3a752ec2596dc23bd0a317b57ea
Matheos <matheos.zaharopoulos@gmail.com>
2020-03-29 22:25:10 -0400

    loader feature added, dataframe modified to work with whole files split by set length

--- src/audio_classifier/create_dataframe_v1.py
+++ src/audio_classifier/create_dataframe_v1.py
@@ -10,6 +10,7 @@ import numpy as np
 from tqdm import tqdm
 from kymatio.numpy import Scattering1D
 
+from src.audio_classifier.loader import _loader
 
 path = 'samples_data'
 
@@ -67,8 +68,8 @@ def _return_tempo(x, sr):
 
 
 @pytest.mark.slow
-def create_dataframe(path_to_data_directory=path, save: str = '',
-                     verbose_prog: bool = False):
+def create_dataframe(path_to_data_directory=path, max_len: int = 30, 
+                     save: str = '', verbose_prog: bool = False):
     """
     Create a dataframe of audio features in pandas.
 
@@ -106,54 +107,52 @@ def create_dataframe(path_to_data_directory=path, save: str = '',
                   f' (code: {idx}) -> {len(files)}\n')
             files = tqdm(files)
         for audio in files:
-            name = audio
             target = int(idx)
-            x, sr = librosa.load(os.path.join(path_to_data_directory,
-                                 category, audio))
-            spectral_centroids = _return_spec_centroid(x, sr)
-            chromagram = pd.Series(librosa.feature.chroma_stft(x, sr=sr)[0])
-            rms = pd.Series(librosa.feature.rms(x)[0])
-            tonnetz = _return_tonnetz(x, sr)
-            contrast = _return_contrast(x, sr)
-            tempo = _return_tempo(x, sr)
-            # wavlet = make_wavlet(x)
-            mfcc = _return_mfcc(x, idx, name)
-            row = {
-                   'file': name,
-                   'chromagram': chromagram,
-                   'spectral_centroids': spectral_centroids,
-                   'tempo': tempo,
-                   'contrast': contrast,
-                   'rms': rms,
-                   'tonnetz': tonnetz,
-                   'target': target
-            }
-            data_frame = pd.DataFrame(row).astype({'target': 'int16',
-                                                  'file': 'category'})
-            data_frame = data_frame.apply(pd.to_numeric, errors='ignore',
-                                          downcast='float')
-            data_frame.loc[:, 'tempo'].fillna(method='ffill',
-                                              downcast='float32',
-                                              inplace=True)
-            data_frame.set_index(['file', 'target'], inplace=True)
-            data_frame = pd.merge(data_frame, mfcc, left_index=True,
-                                  right_index=True, how='outer')
-            data_frame.set_index([data_frame.index, 'time'], inplace=True)
-            data_frame = data_frame.swaplevel(1, 2)
-            data_frame = data_frame.groupby(['file',
-                                             'time',
-                                             'target'])
-            data_frame = data_frame.resample('1S', level=1).mean()
-            data_frame = data_frame.droplevel(3)
-            data_frame.reset_index(inplace=True)
-            data_frame['time'] = data_frame.time.dt.nanosecond
-            data_frame['time'] = data_frame['time'].astype('float32')
-            data_frame['target'] = data_frame['target'].astype('int16')
-            data_frame.set_index('file', inplace=True)
-            # data_frame = data_frame.rolling(50, center=True,
-            #                                 win_type='bartlett',
-            #                                 on='time').mean()
-            main_frame = main_frame.append(data_frame)
+            full_x, sr = _loader(os.path.join(path_to_data_directory,
+                                 category, audio), max_len)
+            for split_num, x in enumerate(list(full_x.values())):
+                name = audio
+                spectral_centroids = _return_spec_centroid(x, sr)
+                chromagram = pd.Series(librosa.feature.chroma_stft(x,
+                                                                   sr=sr)[0])
+                rms = pd.Series(librosa.feature.rms(x)[0])
+                tonnetz = _return_tonnetz(x, sr)
+                contrast = _return_contrast(x, sr)
+                tempo = _return_tempo(x, sr)
+                mfcc = _return_mfcc(x, idx, name)
+                row = {'file': name,
+                       'chromagram': chromagram,
+                       'spectral_centroids': spectral_centroids,
+                       'tempo': tempo,
+                       'contrast': contrast,
+                       'rms': rms,
+                       'tonnetz': tonnetz,
+                       'target': target
+                       }
+                data_frame = pd.DataFrame(row).astype({'target': 'int16',
+                                                       'file': 'category'})
+
+                data_frame = data_frame.apply(pd.to_numeric, errors='ignore',
+                                              downcast='float')
+                data_frame.loc[:, 'tempo'].fillna(method='ffill',
+                                                  downcast='float32',
+                                                  inplace=True)
+                data_frame.set_index(['file', 'target'], inplace=True)
+                data_frame = pd.merge(data_frame, mfcc, left_index=True,
+                                      right_index=True, how='outer')
+                data_frame.set_index([data_frame.index, 'time'], inplace=True)
+                data_frame = data_frame.swaplevel(1, 2)
+                data_frame = data_frame.groupby(['file',
+                                                'time',
+                                                'target'])
+                data_frame = data_frame.resample('1S', level=1).mean()
+                data_frame = data_frame.droplevel(3)
+                data_frame.reset_index(inplace=True)
+                data_frame['time'] = data_frame.time.dt.nanosecond
+                data_frame['time'] = data_frame['time'].astype('float32')
+                data_frame['target'] = data_frame['target'].astype('int16')
+                data_frame.set_index('file', inplace=True)
+                main_frame = main_frame.append(data_frame)
     main_frame = main_frame.sort_index()
     main_frame = main_frame[main_frame.columns].apply(_normalize, axis=0)
     main_frame.index = main_frame.index.astype('category')
